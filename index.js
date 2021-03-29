d2641f515e01c72a978e8b7adfc06db2749e91d1
svenWacker <s.u.wacker@outlook.de>
2021-03-29 12:10:17 +0200

    credo in index.js and facicon

--- index.js
+++ index.js
@@ -3,5 +3,24 @@ let year = new Date().getFullYear();
 document.querySelector(".year").innerHTML = year;
 
 // random credo string
-let credo = "I am proud of my work";
-document.querySelector(".credo").innerHTML = credo;
+var credos = [
+  "I am proud of my work",
+  "Perfection is for everybody different",
+  "Be kind to yourself",
+  "Find your voice",
+  "Become a multi-lingual thinker",
+  "Have a client service mentality",
+  "Listen with intent",
+  "Consider the safety and wellbeing of the planet and its citizens",
+  "Work across all scales",
+  "None of us is as smart as all of us",
+  "Let ideas breathe",
+  "Dive deep into problem solving",
+  "Package every idea with a purpose",
+];
+function sentence() {
+  var random = Math.floor(Math.random() * credos.length);
+  credo = credos[random];
+  document.querySelector(".credo").innerHTML = credo;
+}
+sentence();
