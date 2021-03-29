207c89ec69fb886599fb61ebadffc76e4f6536c7
svenWacker <s.u.wacker@outlook.de>
2021-03-29 11:11:53 +0200

    credo in index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+// picking the actual year for the footer
+let year = new Date().getFullYear();
+document.querySelector(".year").innerHTML = year;
+
+// random credo string
+let credo = "I am proud of my work";
+document.querySelector(".credo").innerHTML = credo;
